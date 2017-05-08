31cc67e20c37e923c7f67e65eb4a5038ad301419
cauil <xiaoloujun@gmail.com>
2017-05-09 00:14:41 +0800

    modify user

--- src/components/user.js
+++ src/components/user.js
@@ -4,15 +4,35 @@ import {
     View,
     Text,
     Image,
+    Button,
     Linking,
     TouchableHighlight, 
 } from 'react-native';
+
+const CookieManager = require('react-native-cookies');
 
 export default class User extends Component {
     constructor(props) {
         super(props);
         this.state = {
         };
+    }
+    componentDidMount() {
+        CookieManager.get(home_url, (err, cookie) => { // 判断cookie
+          let isAuthenticated;
+          console.log(cookie);
+          if (cookie && cookie.hasOwnProperty('user')) {
+            isAuthenticated = true;
+          }
+          else {
+            isAuthenticated = false;
+          }
+
+          this.setState({
+            loggedIn: isAuthenticated,
+            loadedCookie: true
+          });
+        });
     }
     render() {
         return (
@@ -21,6 +41,12 @@ export default class User extends Component {
               <Text style={Style.ad}>
                     The mobile guanggoo app created by reactnative。
               </Text>
+              <Button
+                  onPress={this._onSelect}
+                  title="退出登陆"
+                  color="#841584"
+                  accessibilityLabel="Learn more about this purple button"
+                />
             <TouchableHighlight onPress={() => this._onPress('http://github.com/cauil/react-native-guanggoo')}>
               <Text style={Style.link}>
                 http://github.com/cauil/react-native-guanggoo
@@ -28,6 +54,13 @@ export default class User extends Component {
             </TouchableHighlight>
           </View>
         );
+    }
+    _onSelect() {
+        CookieManager.clearAll((err, res) => {
+          console.log('cookies cleared!');
+          console.log(err);
+          console.log(res);
+        });
     }
     _onPress(url){
         Linking.openURL(url);
