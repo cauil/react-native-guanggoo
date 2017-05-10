636eacd0c817d7c664cdf637a03b72795c2254b7
cauil <xiaoloujun@gmail.com>
2017-05-11 02:22:55 +0800

    add code-push

--- index.ios.js
+++ index.ios.js
@@ -12,12 +12,36 @@ import {
   View
 } from 'react-native';
 
+import codePush from "react-native-code-push";
 import Load from './src/components/load.js';
 import Main from './src/main_ios';
 
 export default class Home  extends Component {
     state = {
         loaded: false,
+    }
+
+    codePushStatusDidChange(status) {
+        switch(status) {
+            case codePush.SyncStatus.CHECKING_FOR_UPDATE:
+                console.log("Checking for updates.");
+                break;
+            case codePush.SyncStatus.DOWNLOADING_PACKAGE:
+                console.log("Downloading package.");
+                break;
+            case codePush.SyncStatus.INSTALLING_UPDATE:
+                console.log("Installing update.");
+                break;
+            case codePush.SyncStatus.UP_TO_DATE:
+                console.log("Up-to-date.");
+                break;
+            case codePush.SyncStatus.UPDATE_INSTALLED:
+                console.log("Update installed.");
+                break;
+        }
+    }
+    codePushDownloadDidProgress(progress) {
+        console.log(progress.receivedBytes + " of " + progress.totalBytes + " received.");
     }
 
     componentDidMount() {
@@ -40,5 +64,18 @@ export default class Home  extends Component {
     }
 }
 
+Home = codePush({ 
+    updateDialog: {
+        appendReleaseDescription: true,
+        descriptionPrefix: '更新: ',
+        mandatoryContinueButtonLabel: '确定',
+        mandatoryUpdateMessage: '',
+        optionalIgnoreButtonLabel: '取消',
+        optionalInstallButtonLabel: '确定',
+        optionalUpdateMessage: '',
+        title: '可用更新',
+    },
+    installMode: codePush.InstallMode.ON_NEXT_RESUME 
+})(Home);
 
 AppRegistry.registerComponent('guanggoo', () => Home);
