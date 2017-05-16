51fcbd7d2dccb07dd094a19361500260cdc6e52d
cauil <xiaoloujun@gmail.com>
2017-05-16 10:44:31 +0800

    modify

--- src/components/topic.js
+++ src/components/topic.js
@@ -20,7 +20,6 @@ import {getHtml} from '../utils/api';
 import {parseTopicData} from '../utils/data';
 import {needLoginNodes} from '../utils/const';
 import {myToast} from '../utils/helper'
-import {Style} from './topicSheet';
 
 const CookieManager = require('react-native-cookies');
 const home_url = 'http://www.guanggoo.com';
@@ -268,3 +267,71 @@ export default class Topic extends Component {
     }
 }
 
+const Style = StyleSheet.create({
+	container: {
+		flex:1,
+		backgroundColor: '#ffffff',
+	},
+	header: {
+		flexDirection: 'row',
+		backgroundColor: '#f5f5f5',
+		borderColor: '#e9e9e9',
+		borderBottomWidth: 1,
+		padding: 5,
+	},
+	title: {
+        color: '#444',
+		fontSize: 16,
+		fontWeight: 'bold',
+		textAlign: 'left'
+	},
+	info: {
+		fontSize: 12,
+		color: '#666666',
+		textAlign: 'left',
+		marginTop: 5,
+	},
+	avatar: {
+		width: 50,
+		height: 50,
+		marginLeft: 5,
+		borderRadius: 4
+	},
+	content_img: {
+        width: 200,
+        height: 200,
+        resizeMode: 'contain',
+	},
+	content_emoji: {
+        width: 20,
+        height: 20,
+        marginLeft: 10,
+        resizeMode: 'contain',
+	},
+	contentWrapper: {
+        display: 'flex',
+		borderBottomWidth: 1,
+		borderColor: '#cccccc',
+	},
+	content: {
+        flex: 1,
+		fontSize: 14,
+		color: '#666666',
+		padding: 10,
+	},
+	node_name: {
+		backgroundColor: '#f5f5f5',
+		color: '#778087',
+		marginRight: 5
+	},
+	user: {
+		color: '#666666',
+		marginRight: 5,
+		marginLeft: 5
+	},
+    centering: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 8,
+    },
+});
